f17187feeaab84d5b8e2494908cd099f1403ad01
HalimMarmarac <halim.kc@gmail.com>
2016-04-08 14:25:17 +0200

    prosjek brojeva u nizu

--- src/Metode.java
+++ src/Metode.java
@@ -8,5 +8,13 @@ public class Metode {
 		}
 		return sum;
 	}
+	
+	public static double average(double[] n) {
+		double sum = 0;
+		for (int i = 0; i < n.length; i++) {
+		    sum += n[i];
+		}
+		return sum/n.length;
+	}
 
 }
